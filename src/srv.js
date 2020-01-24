d2466fc7ccea0d1d7f867d2b6fb83b474951f344
Nox <admin@noxlock.dev>
2020-01-24 21:35:12 +1100

    working with forms

--- src/srv.js
+++ src/srv.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
+
+
 const app = express()
 const port = 3000
 
@@ -7,11 +10,13 @@ const port = 3000
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 
 app.get("/", (req, res) => {
     res.render('index.pug')
+    console.log(req.query.select)
 })
 
 
